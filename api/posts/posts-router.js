2e0853eb7344d67a646441d108c974ac4752a11e
Calvin K <calvinknaub@gmail.com>
2023-12-07 14:34:20 -0800

    wip PUT

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -72,7 +72,69 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {});
+// router.put("/:id", (req, res) => {
+//   const { title, contents } = req.body;
+//   if (!title || !contents) {
+//     res.status(400).json({
+//       message: "Please provide title and contents for the post",
+//     });
+//   } else {
+//     Post.findById(req.params.id)
+//       .then((updatedPost) => {
+//         if (!updatedPost) {
+//           res.status(404).json({
+//             message: "The post with the specified ID does not exist",
+//           });
+//         } else {
+//           return Post.update(req.params.id, req.body);
+//         }
+//       })
+//       .then((data) => {
+//         if (data) {
+//           return Post.findById(req.params.id);
+//         }
+//       })
+//       .then((post) => {
+//         if (post) {
+//           res.json(post);
+//         }
+//       })
+//       .catch((err) => {
+//         res.status(500).json({
+//           message: "The post information could not be modified",
+//         });
+//       });
+//   }
+// });
+router.put("/:id", async (req, res) => {
+  try {
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+      return res.status(400).json({
+        message: "Please provide title and contents for the post",
+      });
+    }
+
+    const updatedPost = await Post.findById(req.params.id);
+
+    if (!updatedPost) {
+      return res.status(404).json({
+        message: "The post with the specified ID does not exist",
+      });
+    }
+
+    await Post.update(req.params.id, req.body);
+    const post = await Post.findById(req.params.id);
+
+    if (post) {
+      return res.json(post);
+    }
+  } catch (err) {
+    return res.status(500).json({
+      message: "The post information could not be modified",
+    });
+  }
+});
 
 router.get("/:id/messages", (req, res) => {});
 
