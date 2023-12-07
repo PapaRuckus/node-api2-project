877fff97f03834c74ef75184cfa3a3af237feee8
Calvin K <calvinknaub@gmail.com>
2023-12-07 13:31:59 -0800

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,26 +1,43 @@
-// implement your posts router here
-const express = require('express')
+const express = require("express");
+const Post = require("./posts-model");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json('foo')
-})
-router.get('/:id', (req, res) => {
+router.get("/", (req, res) => {
+  Post.find()
+    .then((found) => {
+      res.json(found);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "The posts information could not be retrieved",
+      });
+    });
+});
 
-})
-router.post('/', (req, res) => {
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist",
+      });
+    } else {
+      res.json(post);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The post information could not be retrieved",
+    });
+  }
+});
 
-})
-router.delete('/:id', (req, res) => {
+router.post("/", (req, res) => {});
 
-})
-router.put('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {});
 
-})
-router.get('/:id/messages', (req, res) => {
+router.put("/:id", (req, res) => {});
 
-})
+router.get("/:id/messages", (req, res) => {});
 
-
-module.exports = router
+module.exports = router;
